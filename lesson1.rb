0384e5e6bb164c4989c85790bb00779329ae0395
VladZelenko <Vlad6555@ukr.net>
2017-10-15 23:08:12 +0300

    add hw

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,18 @@
 require 'date'
 class Lesson1
-  def sum(val = 0)
-    # TODO
+  def sum(val)
+    val.to_s.split(//).sum(&:to_i)
   end
 
   def age(birthday)
-    # TODO
+    a = (Date.today - Date.parse(birthday)).to_i
+    "Я живу #{a / 365} года или #{a}
+    дней или #{a * 24} часов или
+    #{a * 1440} минут или #{a * 24 * 3600} секунд"
   end
 
   def name
-    # TODO
+    a = gets
+    p "Hello #{a} !"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -7,57 +7,84 @@ class MyArray
 
   def size
     # TODO
+    @array.size
+    # @array.length
   end
 
   def reverse
     # TODO
+    @array.reverse
   end
 
   def max
     # TODO
+    @array.max
   end
 
   def min
     # TODO
+    @array.min
   end
 
   def desc
     # TODO
+    @array.sort.reverse
   end
 
   def asc
     # TODO
+    @array.sort
   end
 
   def odd
     # TODO
+    @array.select(&:odd?)
   end
 
   def multiple_to_three
     # TODO
+    @array.each do |i|
+      (i % 3).zero?
+    end
   end
 
   def uniq
     # TODO
+    @array.uniq
   end
 
   def devide_on_ten
     # TODO
+    @array.map { |a| a / 10.0 }
   end
 
   def chars
     # TODO
+    a = (:a..:z).to_a
+    @array.map { |i| a[i - 1] }
   end
 
   def switch
     # TODO
+    min, max = @array.minmax
+    @array.map do |x|
+      if x == max
+        min
+      elsif x == min
+        max
+      else
+        x
+      end
+    end
   end
 
   def before_min
     # TODO
+    @array.slice(0, @array.rindex(@array.min))
   end
 
   def three_smallest
     # TODO
+    @array.sort.min(3)
   end
 end
